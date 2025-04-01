4dab622ec543bdbfded3e0ea8976f8da8eaee693
fehooaaa228 <dmartenkov2008@gmail.com>
2025-04-01 16:30:31 +0300

    Fix CORS

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+
 app.use(routes)
+
+app.use(cors({
+      origin: "*",
+      methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+    }
+));
 
 
 
